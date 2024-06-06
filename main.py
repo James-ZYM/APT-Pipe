d7bf8bfa348394f5f212b8e940a66ebd42909a4d
James-ZYM <101934745+James-ZYM@users.noreply.github.com>
2024-06-06 14:34:03 +0800

    Code finished
    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import argparse, json, jsonlines, hashlib, tqdm, time
+import argparse, json, jsonlines, hashlib, tqdm, time, copy
 import pandas as pd
 import numpy as np
 from openai import OpenAI
 from googleapiclient import discovery
 from bertopic import BERTopic
 from bertopic.representation import KeyBERTInspired
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from googleapiclient import discovery
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import f1_score
+from sklearn.utils.class_weight import compute_sample_weight
 from transformers import pipeline
+from xgboost import XGBClassifier
 
 sentiment_task = pipeline("sentiment-analysis", model="cardiffnlp/twitter-xlm-roberta-base-sentiment", tokenizer="cardiffnlp/twitter-xlm-roberta-base-sentiment", return_all_scores=True)
 emotion_task = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
@@ -145,7 +147,7 @@ def step_2(file_path, step_1_prompts):
 
     return step_2_prompts
 
-def nlp_injection(metric, row, representation):
+def nlp_injection(metric, row):
     if metric == 'sentiment':
         injection = {
             'Introduction': 'Scores of sentiment leaning of text (ranging from 0 to 1).',
@@ -186,7 +188,7 @@ def nlp_injection(metric, row, representation):
     elif metric == 'topic':
         injection = {
             'Introduction': 'Representative words to describe the major topic of the text.',
-            'Words': representation
+            'Words': row['topic_representation']
         }
 
     return injection
@@ -248,8 +250,9 @@ def step_3(file_path, step_2_prompts):
         if CACHE_FLAG: 
             jsonlines.open('./data_cache/'+hashlib.sha256(file_path.encode()).hexdigest()+' (step_3_topics).jsonl', 'w').write_all(text_topics)
 
-    df_train, df_validation = train_test_split(df_raw, stratify=df_raw['label'], test_size=STEP_3_VALIDATION_SET_PROPORTION, random_state=RANDOM_SEED)
-    
+    df_raw['topic_embedding'] = [topic['embedding'] for topic in text_topics]
+    df_raw['topic_representation'] = [topic['representation'] for topic in text_topics]
+
     tuned_prompts = step_2_prompts
     marked_metrics = []
 
@@ -275,22 +278,142 @@ def step_3(file_path, step_2_prompts):
         if CACHE_FLAG:
             jsonlines.open('./data_cache/'+hashlib.sha256(file_path.encode()).hexdigest()+' (step_3_tuned).jsonl', 'w').write_all([{'marked_metrics': marked_metrics, 'tuned_prompts': tuned_prompts, 'tuned_gpt_labels': tuned_gpt_labels}])
 
-    while True:
-        if len(marked_metrics) == 0:
-            for prompt in tuned_prompts:
-                tmp = json.loads(prompt[1]['content'])
-                tmp['NLP metrics'] = {'Introduction': 'Refer to the following NLP metrics of the text to make classification.'}
-                prompt[1]['content'] = json.dumps(tmp, indent=4)
-        
-        
-        for metric in STEP_3_NLP_METRICS:
-            if metric in marked_metrics:
-                continue
-            break
-        break
+    termination = False
 
-    print(tuned_prompts[0])
-    return
+    while len(marked_metrics) < len(STEP_3_NLP_METRICS) and not termination:
+
+        # =========================================================================
+        # Step 3.1: Split train set and validation set
+        df_raw['tuned_prompt'] = tuned_prompts
+        df_raw['gpt_label'] = tuned_gpt_labels
+        df_raw['gpt_corect'] = df_raw.apply(lambda row: 1 if row['label'].lower()==row['gpt_label'].lower() else 0, axis=1)
+        for label in df_raw['label'].unique():
+            df_raw['one-hot_'+label] = df_raw['gpt_label'].apply(lambda x: 1 if x.lower()==label.lower() else 0)
+        one_hot_cols = [col for col in df_raw.columns if 'one-hot' in col]
+        df_train, df_validation = train_test_split(df_raw, stratify=df_raw['label'], test_size=STEP_3_VALIDATION_SET_PROPORTION, random_state=RANDOM_SEED)
+        
+        # =========================================================================
+        # Step 3.2: Rank NLP Metrics
+
+        fscore_xgb = {}
+        for metric in STEP_3_NLP_METRICS:
+            if metric not in marked_metrics:
+                fscore_xgb[metric] = []
+        X=df_train.drop(columns=['gpt_corect'])
+        y=df_train['gpt_corect']
+        skf = StratifiedKFold(n_splits=10, random_state=RANDOM_SEED, shuffle=True)
+        # skf.get_n_splits(X=X, y=y)
+        for i, (train_index, test_index) in enumerate(skf.split(X, y)):
+            train = df_train.iloc[train_index]
+            test = df_train.iloc[test_index]
+
+            train_set = {}
+            train_set['y'] = train['gpt_corect']
+            for metric in fscore_xgb.keys():
+                if metric == 'sentiment':
+                    train_set[metric] = train[one_hot_cols+['sentiment_negative', 'sentiment_neutral', 'sentiment_positive']]
+                if metric == 'emotion':
+                    train_set[metric] = train[one_hot_cols+['emotion_anger', 'emotion_disgust', 'emotion_fear', 'emotion_joy', 'emotion_neutral', 'emotion_sadness', 'emotion_surprise']]
+                if metric == 'toxicity':
+                    train_set[metric] = train[one_hot_cols+['toxicity_overall', 'toxicity_severe', 'toxicity_identity', 'toxicity_insult', 'toxicity_profanity', 'toxicity_threat']]
+                if metric == 'topic':
+                    one_hot_codes = train[one_hot_cols].to_numpy().tolist()
+                    topic_embeddings = train['topic_embedding'].to_numpy().tolist()
+                    train_set['topic'] = [one_hot_codes[i]+topic_embeddings[i] for i in range(len(one_hot_codes))]
+
+            test_set = {}
+            test_set['y'] = test['gpt_corect']
+            for metric in fscore_xgb.keys():
+                if metric == 'sentiment':
+                    test_set[metric] = test[one_hot_cols+['sentiment_negative', 'sentiment_neutral', 'sentiment_positive']]
+                if metric == 'emotion':
+                    test_set[metric] = test[one_hot_cols+['emotion_anger', 'emotion_disgust', 'emotion_fear', 'emotion_joy', 'emotion_neutral', 'emotion_sadness', 'emotion_surprise']]
+                if metric == 'toxicity':
+                    test_set[metric] = test[one_hot_cols+['toxicity_overall', 'toxicity_severe', 'toxicity_identity', 'toxicity_insult', 'toxicity_profanity', 'toxicity_threat']]
+                if metric == 'topic':
+                    one_hot_codes = test[one_hot_cols].to_numpy().tolist()
+                    topic_embeddings = test['topic_embedding'].to_numpy().tolist()
+                    test_set['topic'] = [one_hot_codes[i]+topic_embeddings[i] for i in range(len(one_hot_codes))]
+
+            sample_weights = compute_sample_weight(
+                class_weight='balanced',
+                y=train_set['y']
+            )
+
+            for metric in fscore_xgb.keys():
+                xgb = XGBClassifier(objective='binary:logistic', seed=RANDOM_SEED)
+                xgb.fit(X=train_set[metric], y=train_set['y'], sample_weight=sample_weights)
+                fscore_xgb[metric].append(f1_score(xgb.predict(test_set[metric]), test_set['y'], average='weighted'))
+
+        fscore_mean = {}
+        for metric in fscore_xgb:
+            fscore_mean[metric] = np.mean(fscore_xgb[metric])
+        fscore_mean = dict(sorted(fscore_mean.items(), key=lambda item: item[1], reverse=True))
+        ranked_metrics = list(fscore_mean.keys())
+
+        # =========================================================================
+        # Step 3.3: Validate NLP injections
+
+        termination = True
+
+        for metric in ranked_metrics:
+            injected_prompts = []
+            for _, row in df_validation.iterrows():
+                prompt = copy.deepcopy(row['tuned_prompt'])
+                if len(marked_metrics) == 0:
+                    tmp = json.loads(prompt[1]['content'])
+                    tmp['NLP metrics'] = {'Introduction': 'Refer to the following NLP metrics of the text to make classification.'}
+                tmp['NLP metrics'][metric.capitalize()] = nlp_injection(metric, row)
+                prompt[1]['content'] = json.dumps(tmp, indent=4)
+                injected_prompts.append(prompt)
+
+            injection_labels = []
+            for prompt in tqdm.tqdm(injected_prompts):
+                response = openai_client.chat.completions.create(
+                                model=GPT_VERSION,
+                                response_format={ "type": "json_object" },
+                                messages=prompt,
+                                temperature=0
+                            )
+                try:
+                    injection_labels.append(json.loads(response.choices[0].message.content)['Label'])
+                except:
+                    injection_labels.append(np.nan)
+
+            if f1_score(df_validation['label'], injection_labels, average='weighted') > f1_score(df_validation['label'], df_validation['gpt_label'], average='weighted'):
+                
+                termination = False
+
+                # =========================================================================
+                # Step 3.4 and Iteration: Mark metric and go to next iteration
+
+                for prompt in tuned_prompts:
+                    if len(marked_metrics) == 0:
+                        tmp = json.loads(prompt[1]['content'])
+                        tmp['NLP metrics'] = {'Introduction': 'Refer to the following NLP metrics of the text to make classification.'}
+                    tmp['NLP metrics'][metric.capitalize()] = nlp_injection(metric, row)
+                    prompt[1]['content'] = json.dumps(tmp, indent=4)
+
+                tuned_gpt_labels = []
+                for prompt in tqdm.tqdm(tuned_prompts):
+                    response = openai_client.chat.completions.create(
+                                    model=GPT_VERSION,
+                                    response_format={ "type": "json_object" },
+                                    messages=prompt,
+                                    temperature=0
+                                )
+                    try:
+                        tuned_gpt_labels.append(json.loads(response.choices[0].message.content)['Label'])
+                    except:
+                        tuned_gpt_labels.append(np.nan)
+
+                marked_metrics.append(metric)
+                
+                if CACHE_FLAG:
+                    jsonlines.open('./data_cache/'+hashlib.sha256(file_path.encode()).hexdigest()+' (step_3_tuned).jsonl', 'w').write_all([{'marked_metrics': marked_metrics, 'tuned_prompts': tuned_prompts, 'tuned_gpt_labels': tuned_gpt_labels}])
+                break
+    
+    return tuned_prompts
                 
 
 def main():
@@ -306,10 +429,15 @@ def main():
         exit()
 
     step_1_prompts = step_1(file_path, task_domain)
+    print('step_1 finished')
 
     step_2_prompts = step_2(file_path, step_1_prompts)
+    print('step_2 finished')
 
-    step_3(file_path, step_2_prompts)
+    tuned_prompts = step_3(file_path, step_2_prompts)
+    print('step_3 finished')
+
+    jsonlines.open('./output/'+hashlib.sha256(file_path.encode()).hexdigest()+' (tuned_prompts).jsonl', 'w').write_all(tuned_prompts)
     return
 
 if __name__ == "__main__":
